31f9aef887e450974dae4f42e6e8a38077724f8b
Nick Bansal <https://github.com/NickBansal>
2019-01-23 13:43:52 +0000

    Data added to Folders component

--- src/Components/Folders.jsx
+++ src/Components/Folders.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import FoldersData from '../Data'
 
 const Folders = () => {
     return (
         <div>
             <h1>FOlders</h1>
+            {FoldersData.map(folder => {
+                return (
+                    <div>
+                        <p>{folder.type}</p>
+                        <p>{folder.name}</p>
+                        <p>{folder.added}</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
