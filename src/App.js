c7e27b97907f2c83d0f7223ef2cdae4aeaa0b697
Nick Bansal <https://github.com/NickBansal>
2019-01-23 14:50:53 +0000

    state added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import './App.css';
 import Folders from './Components/Folders'
 
 class App extends Component {
+
+  state = {
+    folderItemElement: 0,
+    toggleInformation: false
+  }
+
   render() {
+    const { folderItemElement, toggleInformation } = this.state
     return (
       <div className="App">
-        <Folders />
+        <Folders 
+        folderItemElement={folderItemElement}
+        toggleInformation={toggleInformation}
+        handleClick={this.handleClick}/>
       </div>
     );
   }
+
+  handleClick = index => {
+    this.setState({
+      folderItemElement: index,
+      toggleInformation: !this.state.toggleInformation
+    })
+  }
+
 }
 
 export default App;
--- src/Components/Folders.jsx
+++ src/Components/Folders.jsx
@@ -5,16 +5,16 @@ import pdf from '../Stylesheets/Images/adobe.png'
 import csv from '../Stylesheets/Images/csv.png'
 import fileFolder from '../Stylesheets/Images/folder.png'
 
-const Folders = () => {
+const Folders = ({ handleClick }) => {
     return (
         <div className='Folders'>
-            {FoldersData.map(folder => {
+            {FoldersData.map((folder, index) => {
 
                 const imageSource = folder.type === 'pdf' ? pdf :
                 folder.type === 'csv' ? csv : fileFolder 
 
-                const folderDate = folder.added ? <p className='Folders__added'>{folder.added}</p> : 
-                <i className="fas fa-chevron-right fa-2x"></i>
+                const folderInformation = folder.added ? <p className='Folders__added'>{folder.added}</p> : 
+                <i onClick={() => handleClick(index)} className="fas fa-chevron-right fa-2x"></i>
 
                 const newClassName = folder.added ? 'Folders__information' : 'Folders__toggle'
 
@@ -28,7 +28,7 @@ const Folders = () => {
                         alt={folder.type}/>
                         <div className={newClassName}>
                             <p className='Folders__name'>{folder.name}</p>
-                            {folderDate}
+                            {folderInformation}
                         </div>
                     </div>
                 )
