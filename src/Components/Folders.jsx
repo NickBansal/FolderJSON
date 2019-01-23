2de3eb593bbfbb36d6901a41a0ce7b21336f880c
Nick Bansal <https://github.com/NickBansal>
2019-01-23 14:08:57 +0000

    box shadow added

--- src/Components/Folders.jsx
+++ src/Components/Folders.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import FoldersData from '../Data'
+import '../Stylesheets/Folders.css'
+import pdf from '../Stylesheets/Images/adobe.png'
+import csv from '../Stylesheets/Images/csv.png'
+import fileFolder from '../Stylesheets/Images/folder.png'
 
 const Folders = () => {
     return (
-        <div>
-            <h1>FOlders</h1>
+        <div className='Folders'>
             {FoldersData.map(folder => {
+                const imageSource = folder.type === 'pdf' ? pdf :
+                folder.type === 'csv' ? csv : fileFolder 
                 return (
-                    <div>
-                        <p>{folder.type}</p>
-                        <p>{folder.name}</p>
-                        <p>{folder.added}</p>
+                    <div
+                    className='Folders__item' 
+                    key={folder.name}>
+                        <img 
+                        className='Folders__image'
+                        src={imageSource} 
+                        alt={folder.type}/>
+                        <div className='Folders__information'>
+                            <p className='Folders__name'>{folder.name}</p>
+                            <p className='Folders__added'>{folder.added}</p>
+                        </div>
                     </div>
                 )
             })}
