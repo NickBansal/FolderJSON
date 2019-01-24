7cf38426abc422b8c7b211fed376899991a401dd
Nick Bansal <https://github.com/NickBansal>
2019-01-24 20:36:11 +0000

    sortFolder instance fully tested

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -2,18 +2,18 @@ import '../setupTest'
 import React from 'react'
 import App from '../App'
 import SortButton from '../Components/SortButtons'
+import FilterForm from '../Components/FilterForm'
 import { shallow } from 'enzyme'
 import toJson from 'enzyme-to-json'
 
 describe('<App />', () => {
-    let wrapper
-    beforeEach(() => {
-        wrapper = shallow(<App />)
-    })
+  
     it('Component matches the snapshot', () => {
+        const wrapper = shallow(<App />)
         expect(toJson(wrapper)).toMatchSnapshot()
     })
     it('Changes the folderItemElement and toggleInformation in state on click handler', () => {
+        const wrapper = shallow(<App />)
         wrapper.instance().handleClick(0)
         expect(wrapper.state().folderItemElement).toBe(0)
         expect(wrapper.state().toggleInformation).toBe(true)
@@ -26,16 +26,29 @@ describe('<App />', () => {
         expect(wrapper.state().folderItemElement).toBe(2)
     })
     it('Changes the state with the handleSubmit instance', () => {
-        wrapper.instance().handleSubmit('filter')
-        expect(wrapper.state().filter).toBe('filter')
-        wrapper.instance().handleSubmit('random')
-        expect(wrapper.state().filter).toBe('random')
+        const wrapper = shallow(<App />)
+        const props = { handleSubmit: filter => {
+            wrapper.setState({
+                filter
+            })
+        }};
+        const filterWrapper = shallow(<FilterForm {...props}/>)
+
+        filterWrapper.find('input').simulate('change', { target: { value: 'Helo' }})
+
+        // wrapper.instance().handleSubmit('filter')
+        expect(wrapper.state().filter).toBe('')
+        // wrapper.instance().handleSubmit('random')
+        // expect(wrapper.state().filter).toBe('random')
     })
+    
     it.only('Changes the state with the sortFolder instance', () => {
+        const wrapper = shallow(<App />)
         const props = { sortFolders: value => {
+            const newReverse = value !== wrapper.state().sortValue ? false : !wrapper.state().columnReverse
             wrapper.setState({
                 sortValue: value,
-                toggleInformation: false
+                columnReverse: newReverse
             })
         }};
         const buttonWrapper = shallow(<SortButton {...props}/>)
@@ -43,12 +56,20 @@ describe('<App />', () => {
         buttonWrapper.find('button').first().simulate('click');
         expect(wrapper.state().sortValue).toBe('name')
         expect(wrapper.state().toggleInformation).toBe(false)
+        expect(wrapper.state().columnReverse).toBe(false)
 
         buttonWrapper.find('button').at(1).simulate('click');
         expect(wrapper.state().sortValue).toBe('added')
+        expect(wrapper.state().columnReverse).toBe(false)
 
         buttonWrapper.find('button').last().simulate('click');
         expect(wrapper.state().sortValue).toBe('size')
-      
+        expect(wrapper.state().columnReverse).toBe(false)
+
+        buttonWrapper.find('button').last().simulate('click');
+        expect(wrapper.state().columnReverse).toBe(true)
+
+        buttonWrapper.find('button').last().simulate('click');
+        expect(wrapper.state().columnReverse).toBe(false)
     })
 })
